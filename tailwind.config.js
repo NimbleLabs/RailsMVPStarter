a7d93e10f4b8b5bd2b7b6af7d328d3c8ca3a425d
Harris Reynolds <mhreynolds@gmail.com>
2021-10-06 09:56:45 -0500

    Tailwind and DaisyUI styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,5 +4,20 @@ module.exports = {
     './app/views/**/*.html.erb',
     './app/helpers/**/*.rb',
     './app/javascript/**/*.js'
-  ]
+  ],
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('daisyui'),
+  ],
+  theme: {
+    container: {
+      padding: {
+        DEFAULT: '1rem',
+        sm: '2rem',
+        lg: '4rem',
+        xl: '5rem',
+        '2xl': '6rem',
+      },
+    },
+  },
 }
