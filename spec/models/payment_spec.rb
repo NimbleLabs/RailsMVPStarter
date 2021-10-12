f70d6309c19907ce99539ef6fb08f59c8f3b0494
Harris Reynolds <mhreynolds@gmail.com>
2021-10-12 14:24:56 -0500

    Annotate

--- spec/models/payment_spec.rb
+++ spec/models/payment_spec.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: payments
+#
+#  id                 :bigint           not null, primary key
+#  amount             :integer
+#  description        :string
+#  status             :string
+#  stripe_id          :string
+#  stripe_customer_id :string
+#  created            :integer
+#  user_id            :bigint           not null
+#  created_at         :datetime         not null
+#  updated_at         :datetime         not null
+#
 require 'rails_helper'
 
 RSpec.describe Payment, type: :model do
