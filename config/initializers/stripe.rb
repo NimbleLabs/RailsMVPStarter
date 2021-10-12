5d929e42b83f385eac2052cf313414f2bc8a6257
Harris Reynolds <mhreynolds@gmail.com>
2021-10-12 14:26:32 -0500

    Remove debug

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -3,11 +3,13 @@ StripeEvent.signing_secret = ENV['STRIPE_SIGNING_SECRET'] # e.g. whsec_...
 
 StripeEvent.configure do |events|
 
+  # Define subscriber behavior based on the event object
+  # event.class       #=> Stripe::Event
+  # event.type        #=> "charge.failed"
+  # event.data.object #=> #<Stripe::Charge:0x3fcb34c115f8>
+  
   events.subscribe 'charge.succeeded' do |event|
     charge = event.data.object
-    puts '--------------------------------------------'
-    puts charge.inspect
-    puts '--------------------------------------------'
 
     payment_params = {
       amount: charge.amount,
@@ -29,22 +31,13 @@ StripeEvent.configure do |events|
   end
 
   events.subscribe 'customer.subscription.' do |event|
-    # Define subscriber behavior based on the event object
-    # event.class       #=> Stripe::Event
-    # event.type        #=> "charge.failed"
-    # event.data.object #=> #<Stripe::Charge:0x3fcb34c115f8>
-
     subscription = event.data.object
-    puts '************************************************'
-    puts "Received subscription event, #{event.type}, status #{subscription.status}"
 
     user = User.where(subscription_id: subscription.id).first
     if user.present?
       user.update_columns(subscription_status: subscription.status)
       puts 'User updated'
     end
-
-    puts '************************************************'
 
   end
 
