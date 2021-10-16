3bb40ec02ce57e81ac2e3db25f67fd8c5ce8ce9e
Harris Reynolds <mhreynolds@gmail.com>
2021-10-16 00:48:07 -0500

    Beginning of Beautiful Bootstrap

---
+++ app/controllers/templates_controller.rb
@@ -0,0 +1,53 @@
+class TemplatesController < ApplicationController
+
+  def index
+  end
+
+  def dashboard_sidebar
+  end
+
+  def dashboard_top_nav
+  end
+
+  def modal
+  end
+
+  def page_heading
+  end
+
+  def notifications
+  end
+
+  def form_layout
+  end
+
+  def table
+  end
+
+  def product_list
+  end
+
+  def product_quick_view
+  end
+
+  def product_feature
+  end
+
+  def header_dropdown
+  end
+
+  def feature_sections
+  end
+
+  def header_banner
+  end
+
+  def hero_sections
+  end
+
+  def cta_sections
+  end
+
+  def promo_sections
+  end
+end
