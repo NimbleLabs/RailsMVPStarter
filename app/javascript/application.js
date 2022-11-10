9e3c21736847756796d7e75c4daea23daccb1b62
Harris Reynolds <mhreynolds@gmail.com>
2022-11-09 21:02:29 -0600

    Fix logout and update

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,1 +1,15 @@
 // Entry point for the build script in your package.json
+
+import Rails from "@rails/ujs"
+
+window.Rails = Rails;
+// import Turbolinks from "turbolinks"
+// import * as ActiveStorage from "@rails/activestorage"
+import "channels"
+
+Rails.start()
+// Turbolinks.start()
+// ActiveStorage.start()
+
+import * as bootstrap from "../bootstrap"
+import "../photos"
--- lib/generators/vue_generator.rb
+++ lib/generators/vue_generator.rb
@@ -7,6 +7,7 @@ class VueGenerator < Rails::Generators::NamedBase
     create_details_component
     create_list_component
     create_form_component
+    create_service_class
   end
 
   def create_details_component
@@ -17,9 +18,7 @@ class VueGenerator < Rails::Generators::NamedBase
 
     <div class="card col-12 col-md-6">
       <div class="card-body">
-        <dl>
 #{get_details_data}
-        </dl>
       </div>
     </div>
   </div>
@@ -63,7 +62,11 @@ export default {
   <div>
     <h1>#{class_name.pluralize} List</h1>
 
-    <button class="btn btn-primary" @click="onNew#{class_name}Click">New #{class_name}</button>
+    <div class="mb-3">
+      <router-link :to="{ name: 'new-#{plural_name.singularize}' }" class="btn btn-primary">
+              New #{class_name}
+      </router-link>
+    </div>
 
     <div class="table-responsive">
       <table class="table table-sm">
@@ -104,8 +107,6 @@ export default {
       })
   },
   methods: {
-    onNew#{class_name}Click() {
-    },
     onDeleteClick(#{plural_name.singularize}) {
     },
     onEditClick(#{plural_name.singularize}) {
@@ -131,7 +132,14 @@ export default {
 
     <form>
 #{get_form_fields}
-      <button type="submit" class="btn btn-primary" @click.prevent="onSaveClick">Save</button>
+
+      <div class="mb-3">
+        <button type="submit" class="btn btn-primary" @click.prevent="onSaveClick">Save</button>
+        <router-link :to="{ name: '#{plural_name}' }" class="btn btn-secondary ms-2">
+            Cancel
+        </router-link>
+      </div>
+
     </form>
 
   </div>
@@ -146,7 +154,8 @@ export default {
   data() {
     return {
         #{plural_name.singularize}: {
-#{get_object_attributes}
+          id: null,
+          #{get_object_attributes}
         },
         service: new #{class_name}Service()
     }
@@ -165,6 +174,10 @@ export default {
   methods: {
     
     onSaveClick() {
+        if (this.isFormInvalid()) {
+          return
+        }
+
         let request = {
             #{plural_name.singularize}: this.#{plural_name.singularize}
         }
@@ -179,7 +192,140 @@ export default {
             console.log('error saving #{plural_name.singularize}')
             console.log(message)
         })
+    },
+
+    isFormInvalid() {
+      #{validation_method_body}
     }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
+    FILE
+  end
+
+  def create_modal_form_component
+
+    modelCamelCase = class_name.camelize(:lower)
+
+    create_file "app/javascript/app/views/#{plural_name}/#{class_name}FormModal.vue", <<-FILE
+
+<template>
+  <div id="#{modelCamelCase}FormModal" class="modal fade" tabindex="-1">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-body">
+
+          <div class="d-flex align-items-center p-3">
+            <div class="centered-circle flex-shrink-0 bg-secondary text-primary h-10 w-10">
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+                   stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                   class="feather feather-code small-icon">
+                <polyline points="16 18 22 12 16 6"></polyline>
+                <polyline points="8 6 2 12 8 18"></polyline>
+              </svg>
+            </div>
+            <div class="ms-3">
+              <h3 class="fs-5 spacing-close">{{ actionLabel }} #{class_name}</h3>
+            </div>
+          </div>
+
+          <div v-if="errorMessage" class="alert alert-warning mt-1 mb-3" role="alert">
+            {{ errorMessage }}
+          </div>
+
+          <form>
+            #{get_form_fields}
+          </form>
+
+        </div>
+
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary font-sm" style="min-width: 150px;" @click="onSaveClick">Save
+          </button>
+          <button type="button" class="btn btn-outline-primary font-sm" data-bs-dismiss="modal"
+                  style="min-width: 150px;">Cancel
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+import #{class_name}Service from "../../services/#{class_name}Service";
+
+export default {
+  name: "#{class_name}FormModal",
+  props: {
+      yesEvent: Function,
+      #{class_name.downcase}: Object,
+      actionLabel: String
+    },
+  data() {
+    return {
+      currentModal: null,
+      modalElement: null,
+      errorMessage: null,
+      service: new #{class_name}Service()
+    }
+  },
+  mounted() {
+    this.modalElement = document.getElementById('#{modelCamelCase}FormModal')
+    this.currentModal = new bootstrap.Modal(this.modalElement)
+
+    this.modalElement.addEventListener('hidden.bs.modal', () => {
+      this.$emit('noEvent')
+    })
+
+    this.currentModal.show()
+  },
+  beforeDestroy() {
+    this.modalElement.removeEventListener('hidden.bs.modal')
+  },
+  methods: {
+    
+    onSaveClick() {
+        if (this.isFormInvalid()) {
+          return
+        }
+
+        let request = {
+            #{plural_name.singularize}: this.#{plural_name.singularize}
+        }
+
+        let objectId = this.#{plural_name.singularize}.id
+        let promise = objectId === null ? this.service.create(request) : this.service.update(objectId, request)
+
+        promise.then((response) => {
+            this.yesHandler()
+        }).catch((message) => {
+            console.log('error saving #{plural_name.singularize}')
+            console.log(message)
+        })
+    },
+
+    isFormInvalid() {
+      #{validation_method_body}
+    }
+
+    noHandler(e) {
+      // hide the modal
+      this.currentModal.hide()
+    },
+    yesHandler() {
+      // call the parent's "yesEvent"
+      if (this.yesEvent) {
+        this.yesEvent()
+      }
+      // hide the modal
+      this.noHandler()
+    }
+
   }
 }
 </script>
@@ -206,7 +352,8 @@ export default class #{class_name}Service extends RestService {
     headers = ""
 
     @attributes.each do |attribute|
-      headers += "<th>#{attribute.capitalize}</th>\n"
+      next if attribute == 'id' || attribute == 'created_at' || attribute == 'updated_at' || attribute == 'slug'
+      headers += "<th>#{attribute.titleize}</th>\n"
     end
 
     headers += "<th></th>\n"
@@ -217,13 +364,30 @@ export default class #{class_name}Service extends RestService {
     table_data = ""
 
     @attributes.each do |attribute|
+      next if attribute == 'id' || attribute == 'created_at' || attribute == 'updated_at' || attribute == 'slug'
       table_data += "<td>{{#{plural_name.singularize}.#{attribute}}}</td>\n"
     end
 
-    edit_link += <<-FILE
+    edit_link = <<-FILE
 <td>
-    <router-link :to="{ name: '#{plural_name.singularize}-details', params: {id: #{plural_name.singularize}.id} }">
-      View
+    <router-link :to="{ name: '#{plural_name.singularize}-details', params: {id: #{plural_name.singularize}.id} }" class="me-2">
+      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+                   stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                   class="feather feather-grid">
+                <rect x="3" y="3" width="7" height="7"></rect>
+                <rect x="14" y="3" width="7" height="7"></rect>
+                <rect x="14" y="14" width="7" height="7"></rect>
+                <rect x="3" y="14" width="7" height="7"></rect>
+              </svg>
+    </router-link>
+
+    <router-link :to="{ name: 'edit-#{plural_name.singularize}', params: {id: #{plural_name.singularize}.id} }">
+       <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+                   stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                   class="feather feather-edit">
+                <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path>
+                <path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path>
+              </svg>
     </router-link>
 </td>
     FILE
@@ -236,10 +400,12 @@ export default class #{class_name}Service extends RestService {
     form_fields = ""
 
     @attributes.each do |attribute|
+      next if attribute == 'id' || attribute == 'created_at' || attribute == 'updated_at' || attribute == 'slug'
       form_fields += <<-FILE
       <div class="mb-3">
-          <label for="#{attribute}Input" class="form-label">#{attribute.capitalize}</label>
-          <input type="text" class="form-control" id="#{attribute}Input" v-model="#{plural_name.singularize}.#{attribute}">
+          <label for="#{attribute.camelize(:lower)}Input" class="form-label">#{attribute.humanize}</label>
+          <input type="text" class="form-control" id="#{attribute}Input" v-model="#{plural_name.singularize}.#{attribute}"
+                :class="#{attribute.camelize(:lower)}Invalid ? 'is-invalid' : ''">
       </div>
       FILE
     end
@@ -251,10 +417,10 @@ export default class #{class_name}Service extends RestService {
     details_data = ""
 
     @attributes.each do |attribute|
+      next if attribute == 'id' || attribute == 'created_at' || attribute == 'updated_at' || attribute == 'slug'
       details_data += <<-FILE
           <div>
-            <dt>#{attribute.capitalize}</dt>
-            <dd>{{#{plural_name.singularize}.#{attribute}}}</dd>
+            <strong>#{attribute.titleize}:</strong> {{#{plural_name.singularize}.#{attribute}}}
           </div>
       FILE
     end
@@ -266,9 +432,44 @@ export default class #{class_name}Service extends RestService {
     object_attributes = ""
 
     @attributes.each do |attribute|
+      next if attribute == 'id' || attribute == 'created_at' || attribute == 'updated_at' || attribute == 'slug'
       object_attributes += <<-FILE
           #{attribute}: #{attribute == 'id' ? 'null' : "''"},
       FILE
+    end
+
+    @attributes.each do |attribute|
+      next if attribute == 'id' || attribute == 'created_at' || attribute == 'updated_at' || attribute == 'slug'
+      object_attributes += <<-FILE
+           #{attribute.camelize(:lower)}Invalid: false,
+      FILE
+    end
+
+    object_attributes
+  end
+
+  def validation_method_body
+    object_attributes = ""
+
+    @attributes.each do |attribute|
+      next if attribute == 'id' || attribute == 'created_at' || attribute == 'updated_at' || attribute == 'slug'
+      object_attributes += <<-FILE
+          this.#{attribute.camelize(:lower)}Invalid = this.#{plural_name.singularize}.#{attribute} === '' || this.#{plural_name.singularize}.#{attribute}.trim().length === 0
+      FILE
+    end
+
+    object_attributes += "\n\n return "
+
+    # NOTE... but in code below because we skip some fields but also look at the end index
+
+    @attributes.each_with_index do |attribute, index|
+      next if attribute == 'id' || attribute == 'created_at' || attribute == 'updated_at' || attribute == 'slug'
+      if index === @attributes.length - 1
+        object_attributes += "this.#{attribute.camelize(:lower)}Invalid"
+      else
+        object_attributes += "this.#{attribute.camelize(:lower)}Invalid || "
+      end
+
     end
 
     object_attributes
